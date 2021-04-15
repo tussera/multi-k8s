e947127e2be9dab6aeafb6f6971eb7bce63e16ff
tussera <guilherme.tusso@gmail.com>
2021-04-15 23:11:15 +0200

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@ docker build -t tussitos/multi-client:latest -t tussitos/multi-client:$SHA -f ./
 docker build -t tussitos/multi-server:latest -t tussitos/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t tussitos/multi-worker:latest -t tussitos/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push tussitos/multi-client
-docker push tussitos/multi-server
-docker push tussitos/multi-worker
+docker push tussitos/multi-client:latest
+docker push tussitos/multi-server:latest
+docker push tussitos/multi-worker:latest
 
 docker push tussitos/multi-client:$SHA
 docker push tussitos/multi-server:$SHA
 docker push tussitos/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl ser image deployments/client-deployment client=tussitos/multi-client:$SHA
-kubectl ser image deployments/server-deployment server=tussitos/multi-server:$SHA
-kubectl ser image deployments/worker-deployment worker=tussitos/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=tussitos/multi-client:$SHA
+kubectl set image deployments/server-deployment server=tussitos/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=tussitos/multi-worker:$SHA
